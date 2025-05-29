541713eda1b627be8fc28f7ab29a202242820103
VITOR <vitorsilvio2008@gmail.com>
2025-05-29 16:12:40 -0300

    Add files via upload

--- arrayphp/arrayphp/inicial.php
+++ arrayphp/arrayphp/inicial.php
@@ -115,60 +115,119 @@ button:active {
             float: right;
         }
     </style>
-        <hr>
+    <html>
+        
+    </html><?php
+session_start();
+if(!isset($_SESSION['usuarios'])){
+header("Locatio: index.php");
+}
+if(!isset($_SESSION['nomes'])){
+    $emails = json_decode(file_get_contents("email.json"), true);
+    $senhas = json_decode(file_get_contents("senha.json"), true);
+    $nomes = json_decode(file_get_contents("nome.json"), true);
+    $generos = json_decode(file_get_contents("genero.json"), true);
+    $id = array_search($_SESSION['usuario'], $emails);
+    $_SESSION['nomes']= $nomes;
+    $_SESSION['senhas']= $senhas;
+    $_SESSION['generos']= $generos;
+    $_SESSION['emails']= $emails;
+}
+else{
+    $emails = $_SESSION['emails'];
+    $id = array_search($_SESSION['usuario'], $emails);
+    $nomes = $_SESSION['nomes'];
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale-1">
+        <meta http-equiv="content-language" content="pt-br">
+        <title>PHP / Array</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </head>
+    <style>
+        body {
+            background-color: #000000;
+            color:black;
+        }
+        .user {
+            float: right;
+        }
+
+    </style>
+    <body class="container">
         <hr/>
         <nav>
-           &nbsp;&nbsp;<a href="inicial.php" style="color: white; text-decoration: none">HOME |</a><a href="listagem.php" style="color: white; text-decoration: none"> PRODUTOS|</a><a href="gravar.php" style="color: white; text-decoration: none"> SALVAR DADOS | </a><a href="registrodiario.php" style="color white; text-decoration; none"> REGISTRO DIARIO</a> 
+           &nbsp;&nbsp;<a href="inicial.php" style="color: white; text-decoration: none">HOME |</a><a href="listagem.php" style="color: white; text-decoration: none"> PRODUTOS|</a><a href="gravar.php" style="color: white; text-decoration: none"> SALVAR DADOS | </a><a href="registrodiario.php" style="color white; text-decoration; none"> REFUGO</a> 
            <div class="user">
                 <b style="color: white"><?php echo $nomes[$id]; ?> |</b> <a href="sair.php" style="color: white; text-decoration: none">SAIR</a>&nbsp;&nbsp;
            </div>
         </nav>
         <br/><br/>
-        <center><!-- From Uiverse.io by mrhyddenn --> 
-<button>
-    CADASTRO DE PRODUTOS
-</button>
+        <center><button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <b>CADASTRAR NOVO USUÁRIO</b>
         </button></center>
-      
-
         <br/><br/>
-      
+        <div class="row justify-content-center row-cols-2 row-cols-md-3 text-center">
+            <div class="cols">
+                <div class="card mb-4 rounded shadow-sw">
+                    <div class="card-header py-3">
+                    <h3><b>USUÁRIOS</b></h3>
+                    </div>
+                    <div class="card-body">
+                        <?php
+                            include "usuarios.php";
+                        ?>
+                    </div>
+                </div>
+            </div>
+            <div class="cols">
+                <div class="card mb-4 rounded shadow-sw">
+                    <div class="card-header py-3">
+                       <h3><b>GENEROS</b></h3>
+                    </div>
+                    <div class="card-body">
+                        <?php
+                            include "generos.php";
+                        ?>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="container-fluid">
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">CADASTRAR USUÁRIO</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">CADASTRAR USUARIO</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                <div class="modal-body text-start">
-                    <form action="cadastro.php" method="post">
-                        <label class="form-label">NOME</label>
-                        <input class="form-control" type="text" name="nome" required placeholder="Digite o seu nome"/>
+                <div class="modal-body text - start">
+                    <form action= "cadastro.php" method="post">
+                        <label class="form-lable">NOME</label>
+                        <input class="form-control" type="text" name="nome" required placeholder="Digite o seu nome">
                         <br/>
-                        <label class="form-label">E-MAIL</label>
-                        <input class="form-control" type="email" name="email" required placeholder="Digite o seu e-mail"/>
+                        <label class="form-lable">GENERO</label>
+                        <select class="form-select" aria-label="Selecione seu genero" name="genero" required>
+                      <option value="Masculino">masculino</option>
+                      <option value="Feminino">feminino</option>
+                      <option value="Outro">outro</option>
+                      </select>
                         <br/>
-                        <label class="form-label">GENERO</label>
-                        <select class="form-select" aria-label="Selecione um genero" name="genero" required>
-                            <option selected>Selecione um genero</option>
-                            <option value="Masculino">Masculino</option>
-                            <option value="Feminino">Feminino</option>
-                            <option value="Outro">Outro</option>
-                        </select>
+                        <label class="form-lable">E-MAIL</label>
+                        <input class="form-control" type="email" name="email" required placeholder="Digite o seu e-mail">
                         <br/>
-                        <label class="form-label">SENHA</label>
-                        <input class="form-control" type="password" name="senha" required placeholder="Digite a sua senha"/>
+                        <label class="form-lable">SENHA</label>
+                        <input class="form-control" type="password" name="senha" required placeholder="Digite a sua senha">
                         <br/>
-                        <input type="submit" class="btn btn-success" value="CADASTRAR"/>
-                    </form>
+                        <input type="submit";
+                    <form>
+                    ...
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">FECHAR</button>
+                    <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">FECHAR</button>
                 </div>
             </div>
         </div>
