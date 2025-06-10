b2068743b916aef37e69c43f0aeb33a2a243af75
VITOR <vitorsilvio2008@gmail.com>
2025-06-10 13:57:18 -0300

    Delete arrayphp directory

--- arrayphp/json.php
+++
@@ -1,5 +0,0 @@
-<?php
-$generos = ['o','i','u','y'];
-$json = json_encode($generos, JSON_PRETTY_PRINT);
-file_put_contents("genero.json", $json);
-?>
--- arrayphp/usuarios.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $gen = $_SESSION['generos'];
-    $num = count($gen);
-    echo "<b style='font-size: 127px;'>".$num."</b>";
-?>
